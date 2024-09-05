df799feb09387fc0d3832ebbf20d58ac0a07179a
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-09-05 13:11:29 +0530

    starting

---
+++ backend/controllers/qrCode.js
@@ -0,0 +1,24 @@
+// Import required modules
+import crypto from 'crypto';
+import QRCode from 'qrcode';
+
+// Function to generate a hash value
+function generateHash(input) {
+    return crypto.createHash('sha256').update(input).digest('hex');
+}
+
+// Function to generate QR code
+function generateQRCode(data) {
+    QRCode.toFile('qrcode.png', data, function (err) {
+        if (err) throw err;
+        console.log('QR Code generated and saved as qrcode.png');
+    });
+}
+
+// Input value for hashing
+const input = 'Some data to hash';
+const hashedValue = generateHash(input);
+console.log(`Hashed Value: ${hashedValue}`);
+
+// Generate the QR code for the hashed value
+generateQRCode(hashedValue);
